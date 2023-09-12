e352c056844c8938d85425f8a23eec33376d2077
Martin Cespedes <mcespedescubillos@gmail.com>
2023-09-12 20:47:35 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './errors/custom-error';
 export * from './errors/db-error';
 export * from './errors/validation-error';
 export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
